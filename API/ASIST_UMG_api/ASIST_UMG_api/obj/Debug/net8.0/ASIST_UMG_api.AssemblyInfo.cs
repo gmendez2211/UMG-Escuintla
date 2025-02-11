cc71fa031bb58d98f15576b368a3be7701957524
gmendez <gmendez.2211@gmail.com>
2025-02-11 15:55:53 -0600

    Cambios

--- API/ASIST_UMG_api/ASIST_UMG_api/obj/Debug/net8.0/ASIST_UMG_api.AssemblyInfo.cs
+++ API/ASIST_UMG_api/ASIST_UMG_api/obj/Debug/net8.0/ASIST_UMG_api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASIST_UMG_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89aa7e9a8305ea8a0a3a06543960c348428656a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f2ef6e585556809a1cfad348b53290ee0abbef1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASIST_UMG_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASIST_UMG_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
